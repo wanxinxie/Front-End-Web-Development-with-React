0fa6acef7ac33e18a37d8804e6d7cec4dfc5d817
wanxinxie719 <wanxinxie719@g.ucla.edu>
2020-09-12 20:00:42 -0700

    components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+render() {
   return (
     <div className="App">
     <Navbar dark color="primary">
@@ -11,8 +20,10 @@ function App() {
          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
        </div>
      </Navbar>
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+}
 }
 
 export default App;
